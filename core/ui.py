df291d84745f5be6618ff4592a6a91d18ec84035
dferdiantnn <dferdiantn@gmail.com>
2026-08-21 04:33:12 +0700

    release: Bump version to v10.2 on GitHub

--- core/ui.py
+++ core/ui.py
@@ -9,7 +9,7 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 
-VERSION = "10.1 (Universal Cross-Platform Edition)"
+VERSION = "10.2 (Universal Cross-Platform Edition)"
 
 SPINNER_FRAMES = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
 
